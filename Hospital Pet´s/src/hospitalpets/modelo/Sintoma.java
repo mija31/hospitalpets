ccce8c65db1ff6ddea0593eab87615c330f8d298
eduarditolima <eduarditolima@6e6b70a8-1e00-4856-db5f-86eb0d52bffb>
2011-07-20 21:28:55 +0000

    Clases Enfermedad y Sintomas

---
+++ Hospital Pet´s/src/hospitalpets/modelo/Sintoma.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package hospitalpets.modelo;
+
+/**
+ *
+ * @author EDU
+ */
+public class Sintoma {
+    private String nombre;
+    private int idSintoma;
+
+    public int getSintoma() {
+        return idSintoma;
+    }
+
+    public void setSintoma(int idSintoma) {
+        this.idSintoma = idSintoma;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+}
